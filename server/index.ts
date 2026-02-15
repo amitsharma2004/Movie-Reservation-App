c41a602f79899fbc6ab8a72a7ac6d1370ecd4a6d
Amit Sharma <148250972+amitsharma2004@users.noreply.github.com>
2026-02-15 12:49:02 +0530

    Change CORS origin to allow all origins

--- server/index.ts
+++ server/index.ts
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 // app.use(corsOptions);
 app.use (cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
