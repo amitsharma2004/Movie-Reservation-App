9df5a5b40bee537810e65e98e958832ec6c5b4e1
Amit Sharma <akps1440236@gmail.com>
2026-02-15 13:03:11 +0530

    debug: allow only specific origin using CORS policy

--- server/index.ts
+++ server/index.ts
@@ -22,12 +22,15 @@ app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 // app.use(corsOptions);
-app.use (cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}))
+app.use(cors({
+  origin: [
+    'https://movie-reservation-app.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 
 // Serve static files from uploads directory
